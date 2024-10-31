3f86f51c1539b91fee3c8f9006cd2854f4297154
motemotech <i.am.nicoshark@gmail.com>
2024-10-31 21:40:51 +0900

    :sparkles: add public HashimotoLight function

--- consensus/ethash/algorithm.go
+++ consensus/ethash/algorithm.go
@@ -407,6 +407,21 @@ func hashimotoLight(size uint64, cache []uint32, hash []byte, nonce uint64) ([]b
 	return hashimoto(hash, nonce, size, lookup)
 }
 
+func HashimotoLight(size uint64, cache []uint32, hash []byte, nonce uint64) ([]byte, []byte) {
+	keccak512 := makeHasher(sha3.NewLegacyKeccak512())
+
+	lookup := func(index uint32) []uint32 {
+		rawData := generateDatasetItem(cache, index, keccak512)
+
+		data := make([]uint32, len(rawData)/4)
+		for i := 0; i < len(data); i++ {
+			data[i] = binary.LittleEndian.Uint32(rawData[i*4:])
+		}
+		return data
+	}
+	return hashimoto(hash, nonce, size, lookup)
+}
+
 // hashimotoFull aggregates data from the full dataset (using the full in-memory
 // dataset) in order to produce our final value for a particular header hash and
 // nonce.
